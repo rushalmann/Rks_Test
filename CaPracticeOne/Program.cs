ab46e2e9e8c6c661907d5473c8c5b6186698f3b2
Rahul <rushalmann@gmail.com>
2024-08-08 20:49:29 +0530

    First commit

---
+++ CaPracticeOne/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaPracticeOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rahul says, Hello Git world.");
+        }
+    }
+}
